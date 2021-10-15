923276467f90dd0ef3b35ddbd149ff32c9ce5d0a
kescha <kescha2@gmx.net>
2021-10-16 00:38:18 +0200

    removed stuff

---
+++ python/source/config.py
@@ -0,0 +1,9 @@
+settings = {
+    'api_key': '',
+    'zip_code': '45128',
+    'country_code': 'de',
+    'temp_unit': 'metric'}  # unit can be metric, imperial, or kelvin
+
+
+def get_config():
+    return settings
--- python/source/weather.py
+++ python/source/weather.py
@@ -1,15 +1,11 @@
 import requests
-
-settings = {
-    'api_key': '***REMOVED***',
-    'zip_code': '45128',
-    'country_code': 'de',
-    'temp_unit': 'metric'}  # unit can be metric, imperial, or kelvin
+from config import get_config
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid={0}&zip={1},{2}&units={3}"
 
 
 def fetch_weather(zip_code):
+    settings = get_config()
     final_url = BASE_URL.format(settings["api_key"], zip_code, settings["country_code"],
                                 settings["temp_unit"])
     weather_data = requests.get(final_url).json()
